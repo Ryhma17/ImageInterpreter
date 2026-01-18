848ea74d7724fbe2d9655094766b0293215c02fb
Joni Sirviö <t3sijo05@students.oamk.fi>
2026-01-18 15:14:12 +0200

    This commit has a component which shows the history pages list data. In root there is a mock data folder for the list

--- screens/HistoryScreen.tsx
+++ screens/HistoryScreen.tsx
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import HistoryItem from '../components/HistoryItem'
+import { TESTI_HISTORIA } from '../testData/testiHistoriaa'
 
 type Props = {}
 
 const HistoryScreen = (props: Props) => {
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
-      <Text>HistoryScreen</Text>
+      <Text style={styles.title}>History</Text>
+      <FlatList
+        data={TESTI_HISTORIA}
+        contentContainerStyle={styles.contentContainer}
+        renderItem={({ item }) => (
+          <HistoryItem
+            image={item.image}
+            date={item.date}
+            location={item.location}
+            title={item.title}
+            subtitle={item.subtitle}
+            onPress={() => {}}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   )
 }
@@ -18,5 +35,16 @@ const styles = StyleSheet.create({
   safeAreaContainer: {
     flex: 1,
     backgroundColor: "#262626"
+  },
+  contentContainer: {
+    padding: 16,
+    backgroundColor: "#262626"
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom: 16,
+    paddingLeft: 16,
   }
 })
